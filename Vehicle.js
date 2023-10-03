48d873d229fba0761e5dc4b568009531a6a87522
huichuan1999 <58366430+huichuan1999@users.noreply.github.com>
2023-10-03 14:04:08 +0100

    add vehicle class

---
+++ Vehicle.js
@@ -0,0 +1,51 @@
+
+class Vehicle {
+    constructor(pos1, pos2, springLength, springStrength) {
+      this.motors = [];
+      this.physics = new toxi.physics2d.VerletPhysics2D();
+  
+      // 创建两个motor对象
+      this.motors[0] = new Motor(pos1.x, pos1.y);
+      this.motors[1] = new Motor(pos2.x, pos2.y);
+  
+      // 将motor的粒子添加到物理世界中
+      this.physics.addParticle(this.motors[0].particle);
+      this.physics.addParticle(this.motors[1].particle);
+      
+      // 创建一个spring，连接两个motor的粒子，并添加到物理世界中
+      let spring = new toxi.physics2d.VerletSpring2D(
+        this.motors[0].particle,
+        this.motors[1].particle,
+        springLength,  
+        springStrength 
+      );
+      this.physics.addSpring(spring);
+    }
+  
+    // 单独的方法用于更新和渲染 Vehicle
+    run(target) {
+      this.physics.update();
+  
+      for (let motor of this.motors) {
+        let flee = motor.flee(target);
+        motor.applyForce(flee);
+  
+        motor.update();
+        motor.randomSwing(0.5);
+        motor.show();
+  
+        pg.point(motor.pos.x, motor.pos.y);
+      }
+  
+      stroke(0);
+      line(this.motors[0].pos.x, this.motors[0].pos.y, this.motors[1].pos.x, this.motors[1].pos.y);
+  
+      noStroke();
+      text(
+        "Motor 0 velocity: (" + this.motors[0].vel.x.toFixed(2) + ", " + this.motors[0].vel.y.toFixed(2) + ") " + "  " +
+        "Motor 1 velocity: (" + this.motors[1].vel.x.toFixed(2) + ", " + this.motors[1].vel.y.toFixed(2) + ")",
+        100,
+        50
+      );
+    }
+  }
--- sketch.js
+++ sketch.js
@@ -1,70 +1,32 @@
-let motors = [];
 let target;
 let pg;
-let physics;
+let vehicles = [];
+let numVehicles = 3;
 
 function setup() {
-  createCanvas(800, 800);
-  pg = createGraphics(800, 800);
+  createCanvas(windowWidth,windowHeight);
+  pg = createGraphics(windowWidth,windowHeight);
 
-  saveButton = createButton("Save Image"); // Create a button
-  saveButton.mousePressed(saveImage); // Set up a callback on button press to save the image
-  
-  for (let i = 0; i < 2; i++) {
-    let motor = new Motor(width/3 + i * 20, height/2);//初始位置
-    motors.push(motor);
+  let saveButton = createButton("Save Image");
+  saveButton.mousePressed(saveImage);
+
+  // 在这里我们只创建 Vehicles 对象
+  for(let i=0;i<numVehicles;i++){
+    vehicle = new Vehicle(createVector(width/3, height/2), createVector(2*width/3, height/2), 200, 0.1);
+    vehicles.push(vehicle);
   }
-  
-  physics = new toxi.physics2d.VerletPhysics2D();
-  physics.addParticle(motors[0].particle);
-  physics.addParticle(motors[1].particle);
-  
-  let spring = new toxi.physics2d.VerletSpring2D(
-    motors[0].particle,
-    motors[1].particle,
-    100,  // 该值为弹簧的长度
-    0.1 // 该值为弹簧的强度
-  );
-  physics.addSpring(spring);
-  
 }
 
 function draw() {
   background(255);
-  physics.update();
-  
+
   target = createVector(mouseX, mouseY);
   fill(0, 100);
   circle(target.x, target.y, 16);
 
-  for (let motor of motors) {
-    // let seek = motor.seek(target);
-    // motor.applyForce(seek);
-
-    let flee = motor.flee(target);
-    motor.applyForce(flee);
-
-    //motor.seek(target);
-    motor.update();
-    motor.randomSwing(0.5);
-    motor.show();
-   // motor.edges();
-
-    pg.point(motor.pos.x, motor.pos.y);
+  for(let vehicle of vehicles){
+    vehicle.run(target); // 传递 target 给 Vehicle 对象，让它决定如何运动
   }
-  
-  stroke(0);
-  line(motors[0].pos.x, motors[0].pos.y, motors[1].pos.x, motors[1].pos.y);
-  
-  noStroke();
-  text(
-  "Motor 0 velocity: (" + motors[0].vel.x.toFixed(2) + ", " + motors[0].vel.y.toFixed(2) + ") " +"  "+
-  "Motor 1 velocity: : (" + motors[1].vel.x.toFixed(2) + ", " + motors[1].vel.y.toFixed(2) + ")", 
-  100, 
-  50
-);
-
-  
   image(pg, 0, 0);
 }
 
